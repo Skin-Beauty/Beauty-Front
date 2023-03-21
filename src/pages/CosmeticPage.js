d26af977bdd87f5852c5c1e2d2e2e483b4b61e16
miinjoo <minjoo9903@ewhain.net>
2023-03-21 12:42:55 +0900

    fix: 메인화면 카테고리 수정

--- src/pages/CosmeticPage.js
+++ src/pages/CosmeticPage.js
@@ -3,6 +3,7 @@ import styled, { createGlobalStyle } from "styled-components";
 import { Link } from "react-router-dom";
 import "../styles/common.scss";
 
+import icons from "../_mock/icons";
 import titleBox from "../components/titleBox";
 import popularCosmetic from "../_mock/popular";
 import BackBtn from "../components/backBtn";
@@ -11,8 +12,20 @@ import SideBar from "../components/sideBar";
 const CosmeticPage = () => {
   return (
     <>
-      <BackBtn></BackBtn>
+      <BackBtn />
       <SideBar />
+      <SelectWrapper>
+        {icons.map((icon) => {
+          return (
+            <SelectBox>
+              <SelectImgBox>
+                <img src={icon.img}></img>
+              </SelectImgBox>
+              <SelectNameBox>{icon.name}</SelectNameBox>
+            </SelectBox>
+          );
+        })}
+      </SelectWrapper>
       {titleBox("인기제품")}
       {popularCosmetic.map((cos) => {
         return (
@@ -29,6 +42,38 @@ const CosmeticPage = () => {
   );
 };
 
+const SelectBox = styled.div`
+  width: fit-content;
+`;
+const SelectNameBox = styled.div`
+  width: fit-content;
+  margin: auto;
+  font-size: 10px;
+`;
+
+const SelectImgBox = styled.div`
+  background: #ffffff;
+  border: 1px solid #d38189;
+  border-radius: 20px;
+  width: 60px;
+  height: 60px;
+  margin: 15px 20px 5px;
+  img {
+    width: 40px;
+    height: 40px;
+    margin: 10px auto;
+    display: block;
+  }
+  &:hover {
+    background-color: #d38189;
+  }
+`;
+const SelectWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: fit-content;
+  margin: 20px auto;
+`;
 const CosmeticWrapper = styled.div`
   border: 1px solid rgba(0, 0, 0, 0.2);
   border-radius: 20px;
